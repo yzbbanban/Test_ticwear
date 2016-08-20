1035a99b6a1ad5c20e1b3982e496b424141d2291
yzbbanban <yzbbanban@live.com>
2016-08-20 10:11:43 +0800

    简单的轮播图

--- wear/src/main/java/com/wangban/yzbbanban/test_ticwear/MainActivity.java
+++ wear/src/main/java/com/wangban/yzbbanban/test_ticwear/MainActivity.java
@@ -3,11 +3,15 @@ package com.wangban.yzbbanban.test_ticwear;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.wearable.view.WatchViewStub;
-import android.widget.TextView;
+
+import java.util.*;
+
+import com.youth.banner.Banner;
 
 public class MainActivity extends Activity {
 
-    private RoundImageView mRoundImageView;
+    private Banner banner;
+    private List<Integer> imgs = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,8 +21,26 @@ public class MainActivity extends Activity {
         stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
             @Override
             public void onLayoutInflated(WatchViewStub stub) {
-                mRoundImageView= (RoundImageView) findViewById(R.id.mRoundImageView);
-                
+                banner= (Banner) findViewById(R.id.banner);
+                imgs.add(R.mipmap.be1);
+                imgs.add(R.mipmap.be2);
+                imgs.add(R.mipmap.be3);
+                imgs.add(R.mipmap.be4);
+                imgs.add(R.mipmap.be5);
+                imgs.add(R.mipmap.be6);
+                imgs.add(R.mipmap.be7);
+                imgs.add(R.mipmap.be8);
+                imgs.add(R.mipmap.be9);
+                imgs.add(R.mipmap.be10);
+                imgs.add(R.mipmap.be11);
+                imgs.add(R.mipmap.be12);
+                imgs.add(R.mipmap.be13);
+                imgs.add(R.mipmap.be14);
+                imgs.add(R.mipmap.be15);
+                imgs.add(R.mipmap.be16);
+                imgs.add(R.mipmap.be17);
+                banner.setImages(imgs);
+
             }
         });
     }
